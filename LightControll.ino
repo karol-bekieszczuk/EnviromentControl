b466b4a479f31de92fb7c168894549b74d2d1258
karol <karol.bekieszczuk@gmail.com>
2025-09-19 20:13:18 +0200

    Add light control

---
+++ LightControll.ino
@@ -0,0 +1,45 @@
+#include <PCF8563.h>
+
+PCF8563 pcf;
+
+const int LIGHT_CTRL_PIN = 2;
+bool isLightOn = false;
+
+void setup() {
+  pinMode(LIGHT_CTRL_PIN, OUTPUT);
+  digitalWrite(LIGHT_CTRL_PIN, LOW);
+  pcf.init();//initialize the clock
+  // pcf.stopClock();//stop the clock
+
+  // //set time to to 31/3/2018 17:33:0
+  // pcf.setYear(18);//set year
+  // pcf.setMonth(3);//set month
+  // pcf.setDay(31);//set dat
+  // pcf.setHour(17);//set hour
+  // pcf.setMinut(33);//set minut
+  // pcf.setSecond(0);//set second
+
+  // pcf.startClock();//start the clock
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  lightControl();
+}
+
+void lightControl()
+{
+  Time nowTime = pcf.getTime();
+  int currentTimeInMinutes = nowTime.hour * 60 + nowTime.minute;
+  int lightStartTime = 630;
+  int darkStartTime = 1290;
+  bool shouldBeOn = (currentTimeInMinutes >= lightStartTime && currentTimeInMinutes < darkStartTime);
+
+  if(shouldBeOn != isLightOn)
+  {
+    isLightOn = shouldBeOn;
+    digitalWrite(LIGHT_CTRL_PIN, isLightOn ? HIGH : LOW);
+    Serial.println(isLightOn ? "HIGH" : "LOW");
+  }
+}
