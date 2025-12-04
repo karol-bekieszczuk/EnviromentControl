005b79782840917d593cf60b451603fb70f57ff4
karol <karol.bekieszczuk@gmail.com>
2025-12-04 21:28:24 +0100

    change time when light should be ON from 11h to 12h. Add libraries folder

--- LightControll.ino
+++ LightControll.ino
@@ -3,10 +3,12 @@
 PCF8563 pcf;
 
 const int LIGHT_CTRL_PIN = 2;
+const int FAN_PIN = 3;
 bool isLightOn = false;
 
 void setup() {
   pinMode(LIGHT_CTRL_PIN, OUTPUT);
+  pinMode(FAN_PIN, OUTPUT);
   digitalWrite(LIGHT_CTRL_PIN, LOW);
   pcf.init();//initialize the clock
   // pcf.stopClock();//stop the clock
@@ -25,12 +27,13 @@ void setup() {
 }
 
 void loop() {
-  lightControl();
+  Time nowTime = pcf.getTime();
+  lightControl(nowTime);
+  //fanControl(nowTime);
 }
 
-void lightControl()
+void lightControl(Time nowTime)
 {
-  Time nowTime = pcf.getTime();
   int currentTimeInMinutes = nowTime.hour * 60 + nowTime.minute;
   int lightStartTime = 630;
   int darkStartTime = 1290;
@@ -42,4 +45,13 @@ void lightControl()
     digitalWrite(LIGHT_CTRL_PIN, isLightOn ? HIGH : LOW);
     Serial.println(isLightOn ? "HIGH" : "LOW");
   }
+}
+
+void fanControl(Time nowTime){
+  // analogWrite(FAN_PIN, 128); // ~50% obrotów
+  // delay(10000);
+  // analogWrite(FAN_PIN, 255); // 100% obrotów
+  // delay(5000);
+  analogWrite(FAN_PIN, 0);   // wentylator niskie obroty
+  delay(5000);
 }
