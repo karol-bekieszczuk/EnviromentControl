ac58377e2e464707cc7f8eba93a2ebd65267f1cf
Karol <karol.bekieszczuk@gmail.com>
2025-12-20 18:42:51 +0100

    added code to run smooth PWC switching, requires change in power control, best switch to relay

--- EnvironmentControl/EnvironmentControl.ino
+++ EnvironmentControl/EnvironmentControl.ino
@@ -1,47 +1,51 @@
 #include <PCF8563.h>
+#include <FanController.h>
 
 PCF8563 pcf;
 
 constexpr int LIGHT_CTRL_PIN = 2;
 constexpr int FAN_POWER_PIN = 5;
 constexpr int FAN_PWM_PIN = 9;
+constexpr int FAN_SENSOR_PIN = 4;
 constexpr int CYCLE_START_MIN = 570; //09:30
 constexpr int CYCLE_STOP_MIN = 1290; //21:30
-const uint16_t TOP = 640; // 16 MHz / 25 kHz TOP - maximum counter value
+// Choose a threshold in milliseconds between readings.
+// A smaller value will give more updated results,
+// while a higher value will give more accurate and smooth readings
+constexpr int FAN_SENSOR_THRESHOLD = 1000;
+
+FanController fan(FAN_SENSOR_PIN, FAN_SENSOR_THRESHOLD, FAN_PWM_PIN);
 
 void setup() {
-  //************** manualy set timers **************//
-  TCCR1A = (1 << COM1A1) | (1 << WGM11);
-  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10);
-
-  ICR1 = TOP;
-  //************** end manualy set timers **************//
   //************** set up pins mode **************//
   pinMode(LIGHT_CTRL_PIN, OUTPUT);
   pinMode(FAN_POWER_PIN, OUTPUT);
-  pinMode(FAN_PWM_PIN, OUTPUT);
+  // pinMode(FAN_PWM_PIN, OUTPUT);
   //************** end set up pins mode **************//
   //************** set up pins default state **************//
   digitalWrite(LIGHT_CTRL_PIN, LOW);
   digitalWrite(FAN_POWER_PIN, LOW);
+  // analogWrite(FAN_PWM_PIN, 0);
   //************** end set up pins default state **************//
   //************** initialize the clock **************//
   pcf.init(); //THIS MUST BE UNCOMMENTED FOR THE RTC TO WORK!!
   
   // set RTC time
-  // pcf.stopClock();//stop the clock
+  pcf.stopClock();//stop the clock
 
-  // pcf.setYear(25);//set year
-  // pcf.setMonth(12);//set month
-  // pcf.setDay(15);//set day
-  // pcf.setHour(16);//set hour
-  // pcf.setMinut(28);//set minut
-  // pcf.setSecond(0);//set second
+  pcf.setYear(25);//set year
+  pcf.setMonth(12);//set month
+  pcf.setDay(15);//set day
+  pcf.setHour(16);//set hour
+  pcf.setMinut(27);//set minut
+  pcf.setSecond(57);//set second
 
-  // pcf.startClock();//start the clock
+  pcf.startClock();//start the clock
   //************** end initialize the clock **************//
 
-  // Serial.begin(9600);
+  fan.begin();
+
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -72,10 +76,38 @@ void fanControl(int currentTimeInMinutes){
   if(shouldBeOn != isFanOn)
   {
     isFanOn = shouldBeOn;
+    Serial.print("Current speed: ");
+    unsigned int rpms = fan.getSpeed(); // Send the command to get RPM
+    Serial.print(rpms);
+    Serial.println("RPM");
+
+  // Get new speed from Serial (0-100%)
+  // if (Serial.available() > 0) {
+    // Parse speed
+    // int input = Serial.parseInt();
+
+    // Constrain a 0-100 range
+    byte target = max(min(isFanOn ? 100 : 0, 100), 0);
+
+    // Print obtained value
+    Serial.print("Setting duty cycle: ");
+    Serial.println(target, DEC);
+
+    // Set fan duty cycle
+    fan.setDutyCycle(target);
+
+    // Get duty cycle
+    byte dutyCycle = fan.getDutyCycle();
+    Serial.print("Duty cycle: ");
+    Serial.println(dutyCycle, DEC);
+    Serial.println(isFanOn);
     digitalWrite(FAN_POWER_PIN, isFanOn ? HIGH : LOW);
-    uint8_t percent = constrain(50, 20, 100); // nie schodź niżej
-    OCR1A = map(percent, 0, 100, 0, TOP);
+    // analogWrite(FAN_PWM_PIN, isFanOn ? 254 : 0);
   }
+
+    // Not really needed, just avoiding spamming the monitor,
+  // readings will be performed no faster than once every THRESHOLD ms anyway
+  delay(250);
 }
 
 void printTime(Time nowTime){
